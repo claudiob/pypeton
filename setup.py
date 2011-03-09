869b682a91cf0445ba63379991099049acd1d174
arkie <email@joncaldwell.com>
2011-03-09 10:50:12 -0800

    formatted, added scripts

--- setup.py
+++ setup.py
@@ -1,34 +1,35 @@
 from distutils.core import setup
 
 setup(
-    name = 'pypeton',
-    packages=['pypeton',],
-	version = 'v0.0.1',
-    author='RED Interactive Agency',
-    author_email='geeks@ff0000.com',
+	name         = 'pypeton',
+	packages     = ['pypeton',],
+	scripts      = ['pypeton/pypeton',],
+	version      = 'v0.0.1',
+	author       = 'RED Interactive Agency',
+	author_email = 'geeks@ff0000.com',
 
-    package_data={
-        'pypeton': [
-            'files/*',
-        ]
-    },
+	package_data = {
+		'pypeton': [
+			'files/*',
+		]
+	},
 
-    url='http://www.github.com/ff0000/pypeton/',
-    download_url = 'http://www.github.com/ff0000/pypeton/',
+	url          = 'http://www.github.com/ff0000/pypeton/',
+	download_url = 'http://www.github.com/ff0000/pypeton/',
 
-    license = 'MIT license',
-    description = """ Tool to start Django and Bongo projects """,
+	license      = 'MIT license',
+	description  = """ Tool to start Django and Bongo projects """,
 
-    long_description = open('README.rst').read(),
-    requires = [],
+	long_description = open('README.rst').read(),
+	requires     = [],
 
-    classifiers = (
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
-    ),
+	classifiers  = (
+		'Development Status :: 3 - Alpha',
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python',
+		'Topic :: Software Development',
+	),
 )

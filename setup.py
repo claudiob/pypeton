66ff63a14394b064d5061e4994852adf1cc672cb
Chris Gilmer <chris.gilmer@ff0000.com>
2011-03-11 11:18:52 -0800

    Removed the package listing in the setup file.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name         = 'pypeton',
-	packages     = ['pypeton',],
+	#packages     = ['pypeton',],
 	scripts      = ['pypeton/pypeton',],
 	version      = 'v0.4.0',
 	author       = 'RED Interactive Agency',

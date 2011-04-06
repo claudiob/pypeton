762c9df5a2080d3541cca41df9279f9dfd244b77
claudiob <claudiob@gmail.com>
2011-04-06 11:43:50 -0700

    ./manage.py server will also clean up .pyc files

--- pypeton/files/django/project/apps/initial_data/management/commands/server.py
+++ pypeton/files/django/project/apps/initial_data/management/commands/server.py
@@ -2,14 +2,18 @@
 
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
+from os import system, environ        
 import logging
 
 class Command(BaseCommand):
-    help = "Call runserver with a given environment."
+    help = "Call runserver with a given environment after removing .pyc files."
 
     def handle(self, *args, **options):
         """
-        Call runserver with a given environment.
+        Call runserver with a given environment after removing .pyc files.
         """
+        system('find . -type f -name "*.pyc" -delete;')
         call_command('runserver', '0.0.0.0:8000')
         logging.info("Require success")
+
+

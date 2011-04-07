claudiob/pypeton

commit 8ea35bd509b4f0c4385faf3a91a91a8ff34ee026

Removed --settings from dumpdata (using DJANGO_SETTINGS_MODULE if required)

pypeton/files/django/project/apps/initial_data/management/commands/dump.py
@@ -20,8 +20,7 @@ class Command(BaseCommand):
         # TODO: Raise error if the parameter is not passed
         model = args[0] 
         options = " --indent=4 --natural"
-        settings = " --settings=settings.%%s" %% env
-        output = "apps/%%s/fixtures/%%s.json" %% (model, env)
-        system('python manage.py dumpdata %%s %%s %%s > %%s' %% 
-            (model, options, settings, output))
+        output = "apps/%s/fixtures/%s.json" % (model, env)
+        system('python manage.py dumpdata %s %s > %s' % 
+            (model, options, output))
         logging.info("Dump success")

commit 560b744dfca3e5a2c88dd2235161c2a08e2a1a7e

Fixed bug introduced in 8ea35bd (I forgot the double %% that are replaced by pypeton into %)

pypeton/files/django/project/apps/initial_data/management/commands/dump.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
         # TODO: Raise error if the parameter is not passed
         model = args[0] 
         options = " --indent=4 --natural"
-        output = "apps/%s/fixtures/%s.json" % (model, env)
-        system('python manage.py dumpdata %s %s > %s' % 
+        output = "apps/%%s/fixtures/%%s.json" %% (model, env)
+        system('python manage.py dumpdata %%s %%s > %%s' %% 
             (model, options, output))
         logging.info("Dump success")

pypeton/files/django/project/urls.py
@@ -18,5 +18,5 @@ urlpatterns += staticfiles_urlpatterns()
 
 # Upload URLS
 if settings.DEBUG:
-    urlpatterns.insert(-2, url(r'^%s(?P<path>.*)' % settings.MEDIA_URL[1:],
+    urlpatterns.insert(-2, url(r'^%%s(?P<path>.*)' %% settings.MEDIA_URL[1:],
         'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))